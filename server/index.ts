55e95fa2d3475a58cc73694c30cd65d0dca43461
Noh4ns3n <rigolot.vincent@gmail.com>
2024-02-10 22:04:58 +0100

    :art: cleanup ][ :sparkles: better useFetch + added routes ][ :construction: began loginForm

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,11 @@
-import express, { Request, Response } from "express";
-import db from "./src/database";
+import express from "express";
+import userRoutes from "./src/routes/user.route";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.use((_, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -11,24 +14,7 @@ app.use((_, res, next) => {
     next();
 });
 
-app.get("/user/:id", async (req : Request, res : Response) => {
-    try {
-        const user : unknown = await new Promise<unknown>((resolve, reject) => {
-            db.get("SELECT * FROM users WHERE id = ?", req.params.id, (err, row) => {
-                if (err) return reject(err);
-                resolve(row);
-            });
-        });
-        if(user) {
-            res.status(200).json({user});
-        } else {
-            res.status(404).json({error : "User not found"});
-        }}
-    catch(error) {
-        console.error(error);
-        res.status(500).json({error : "Internal Service Error"});
-    }
-});
+app.use(userRoutes);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
